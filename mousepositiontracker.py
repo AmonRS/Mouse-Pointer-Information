06bf6c4bda3369b07a4f41cc20e311e4405bf142
Amon <amonsthapit@live.com>
2017-04-22 15:56:55 -0700

    main file

---
+++ mousepositiontracker.py
@@ -0,0 +1,32 @@
+#tracks mouse position (and shows it) for a short(?) time...
+
+import pyautogui
+from tkinter import *
+import time
+
+tk = Tk()   #<var> = Tk()    #create the window to do stuff in
+tk.title('sfsdfgh')
+
+canvas = Canvas(tk, width=450, height=200)
+canvas.pack()
+ct = canvas.create_text(200, 50, text='0000000', fill='red',font=('Courier', 15))
+ct2 = canvas.create_text(200, 100, text='0000000', fill='red',font=('Courier', 15))
+ct3col = canvas.create_rectangle(230,130,250,150, fill='red')
+ct4time = canvas.create_text(350, 180, text='0000000', fill='grey',font=('Courier', 10))
+
+i=0
+while 1==1:
+    pos = pyautogui.position()
+    pixcol = pyautogui.screenshot().getpixel(pos)
+    
+    canvas.itemconfig(ct, text="position: " + str(pos))
+    canvas.itemconfig(ct2, text="color: " + str(pixcol))
+    ccc='#%02x%02x%02x' % (pixcol[0], pixcol[1], pixcol[2])
+    canvas.itemconfig(ct3col, fill=ccc)
+    canvas.itemconfig(ct4time, text=str(time.asctime()))
+    
+    i=i+1
+    if i == 9999999999999999:
+        pyautogui.alert("time's up. sayounara...")
+        break
+    tk.update()
